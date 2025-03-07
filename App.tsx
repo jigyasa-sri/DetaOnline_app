d9662322c918a999a46cd5b9298678d285619d62
jigyasa-sri <jigyasasri47@gmail.com>
2025-03-07 11:41:18 +0530

    Updated safe Area View

--- App.tsx
+++ App.tsx
@@ -1,14 +1,42 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, View } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Image,
+  Platform,
+  StatusBar as RNStatusBar,
+} from "react-native";
 import { WebView } from "react-native-webview";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const statusBarHeight =
+    Platform.OS === "android" ? (RNStatusBar.currentHeight || 0) + 3 : 0;
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   return (
-    <SafeAreaView style={styles.safeArea}>
+    <SafeAreaView style={[styles.safeArea, { paddingTop: statusBarHeight }]}>
       <StatusBar style="auto" />
-      <View style={styles.container}>
-        <WebView source={{ uri: "https://deta.detasys.com" }} />
-      </View>
+
+      {isLoading ? (
+        <View style={styles.splashContainer}>
+          <Image
+            source={require("./assets/asdol_logo.png")}
+            style={styles.splashImage}
+          />
+        </View>
+      ) : (
+        <View style={styles.container}>
+          <WebView source={{ uri: "https://deta.detasys.com" }} />
+        </View>
+      )}
     </SafeAreaView>
   );
 }
@@ -17,6 +45,17 @@ const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
     backgroundColor: "#ffffff",
+  },
+  splashContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ffffff",
+  },
+  splashImage: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
   },
   container: {
     flex: 1,
